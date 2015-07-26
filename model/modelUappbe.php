0475facc7df51e01ce170ce648fce077d4b79db2
fikrifadlillah <fikri.fadlillah@yahoo.com>
2015-07-26 19:17:00 +0700

    fungsi update & delete model uappbe1

--- model/modelUappbe.php
+++ model/modelUappbe.php
@@ -13,6 +13,28 @@ class modelUappbe extends mysql_db
                     	nm_uappbe1='$nm_uappbe1'";
         $result = $this->query($query);
 		return $result;
+	}	
+	public function ubahuapb($data)
+	{
+		$kd_uapb = $data['kd_uapb'];
+		$kd_uappbe1 = $data['kd_uappbe1'];
+		$nm_uappbe1 = $data['nm_uappbe1'];
+		$query = "update uappbe1
+        			set kd_uapb='$kd_uapb',
+        				kd_uappbe1='$kd_uappbe1',
+                    	nm_uappbe1='$nm_uappbe1' where kd_uapb='$kd_uapb' and kd_uappbe1='$kd_uappbe1' ";
+        $result = $this->query($query);
+		return $result;
+	}	
+	public function hapusuapb($data)
+	{
+		$kd_uapb = $data['kd_uapb'];
+		$kd_uappbe1 = $data['kd_uappbe1'];
+		$nm_uappbe1 = $data['nm_uappbe1'];
+		$query = "delete from uappbe1
+        			where kd_uapb='$kd_uapb' and kd_uappbe1='$kd_uappbe1'";
+        $result = $this->query($query);
+		return $result;
 	}
 	public function bacauapb()
 	{
