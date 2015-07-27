612db9f84503f3a3aff0f5e49d1a49d56081b714
titoputra <titoputraprasetyo@gmail.com>
2015-07-27 16:46:10 +0700

    update barang, opsik

--- core/barang/prosesbarang.php
+++ core/barang/prosesbarang.php
@@ -11,25 +11,65 @@ else
 	switch ($manage)
 	{
 		case 'addbarang':
-			$kd_kbrg = $purifier->purify($_POST['kdsskel']);
-			$kd_jbrg = $purifier->purify($_POST['kdbarang']);
-			$kd_brg = $kd_kbrg+$kd_jbrg;
+			$kdbarang = $purifier->purify($_POST['kdbarang']);
+			$kd_jbrg = $purifier->purify($_POST['kd_jbrg']);
+			$kd_brg = $kdbarang+$kd_jbrg;
 			$nm_brg = $purifier->purify($_POST['nmbarang']);
 			$satuan = $purifier->purify($_POST['satuan']);
 
-
-
 			$data = array(
-				"kd_kbrg" => $kd_kbrg,
+				"kd_kbrg" => $kdbarang,
 				"kd_jbrg" => $kd_jbrg,
 				"kd_brg" => $kd_brg,
-				"nm_brg" => $nm_brg,
+				"nm_brg" => $nmbarang,
 				"satuan" => $satuan,
 				"kd_perk" => "sementara",
 		    	"kd_lokasi" => "sementara"
 		    );
 			$Barang->tambahbarang($data);
 		break;
+
+		case 'readksskel':
+				$kdsskel = $purifier->purify($_POST['kdkdsskel']);
+				$Barang->$bacasskel($kdsskel);
+		break;
+
+		case 'ubahbarang':
+			$kdbarang = $purifier->purify($_POST['kdbarang']);
+			$kd_jbrg = $purifier->purify($_POST['kd_jbrg']);
+			$kd_brg = $kdbarang+$kd_jbrg;
+			$nm_brg = $purifier->purify($_POST['nmbarang']);
+			$satuan = $purifier->purify($_POST['satuan']);
+
+			$data = array(
+				"kd_kbrg" => $kdbarang,
+				"kd_jbrg" => $kd_jbrg,
+				"kd_brg" => $kd_brg,
+				"nm_brg" => $nmbarang,
+				"satuan" => $satuan,
+				"kd_perk" => "sementara",
+		    	"kd_lokasi" => "sementara"
+		    );
+			$Barang->ubahbarang($data);
+		break;
+
+		case 'hapusbarang':
+			$kdbarang = $purifier->purify($_POST['kdbarang']);
+			$kd_jbrg = $purifier->purify($_POST['kd_jbrg']);
+			$kd_brg = $kdbarang+$kd_jbrg;
+			$nm_brg = $purifier->purify($_POST['nmbarang']);
+			$satuan = $purifier->purify($_POST['satuan']);
+
+			$data = array(
+				"kd_kbrg" => $kdbarang,
+				"kd_jbrg" => $kd_jbrg,
+				"kd_brg" => $kd_brg,
+				"nm_brg" => $nmbarang,
+				"satuan" => $satuan,
+				"kd_perk" => "sementara",
+		    	"kd_lokasi" => "sementara"
+		    );
+			$Barang->hapusbarang($data);
 		default:
 			echo "Error Data Tidak Tersedia";
 		break;
--- core/opsik/prosesopsik.php
+++ core/opsik/prosesopsik.php
@@ -32,6 +32,53 @@ else
 		    );
 			$Opsik->tambahopsik($data);
 		break;
+
+
+		case 'ubahopsik':
+			$thnang = $purifier->purify($_POST['thnang']);
+			$kdlokasi = $purifier->purify($_POST['kdlokasi']);
+			$kdbrg = $purifier->purify($_POST['kdbrg']);
+			$tglbuku = $purifier->purify($_POST['tgldok']);
+			$nodok = $purifier->purify($_POST['nodok']);
+			$nobukti = $purifier->purify($_POST['nobukti']);
+			$kuantitas = $purifier->purify($_POST['kuantitas']);
+			$rphsat = $purifier->purify($_POST['srphsat']);
+
+			$data = array(
+				"thn_ang" => $thang,
+				"kd_lokasi" => $kdlokasi,
+				"kd_brg" => $kdbrg,
+				"tglbuku" => $tglbuku,
+				"nodok" => $nodok,
+				"no_bukti" => $nobukti,
+		    	"kuantitas" => $kuantitas,
+		    	"rph_sat" => $rphsat
+		    );
+			$Opsik->ubahopsik($data);
+		break;
+
+		case 'hapusopsik':
+			$thnang = $purifier->purify($_POST['thnang']);
+			$kdlokasi = $purifier->purify($_POST['kdlokasi']);
+			$kdbrg = $purifier->purify($_POST['kdbrg']);
+			$tglbuku = $purifier->purify($_POST['tgldok']);
+			$nodok = $purifier->purify($_POST['nodok']);
+			$nobukti = $purifier->purify($_POST['nobukti']);
+			$kuantitas = $purifier->purify($_POST['kuantitas']);
+			$rphsat = $purifier->purify($_POST['srphsat']);
+
+			$data = array(
+				"thn_ang" => $thang,
+				"kd_lokasi" => $kdlokasi,
+				"kd_brg" => $kdbrg,
+				"tglbuku" => $tglbuku,
+				"nodok" => $nodok,
+				"no_bukti" => $nobukti,
+		    	"kuantitas" => $kuantitas,
+		    	"rph_sat" => $rphsat
+		    );
+			$Opsik->hapusopsik($data);
+		break;
 		default:
 			echo "Error Data Tidak Tersedia";
 		break;
