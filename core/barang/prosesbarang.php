fec93da51d60cce6876bfa9960141e102bd52e90
titoputra <titoputraprasetyo@gmail.com>
2015-07-27 15:31:43 +0700

    update proses

--- core/barang/prosesbarang.php
+++ core/barang/prosesbarang.php
@@ -11,24 +11,73 @@ else
 	switch ($manage)
 	{
 		case 'addbarang':
-			$kd_kbrg = $purifier->purify($_POST['kd_kbrg']);
+			$kdbarang = $purifier->purify($_POST['kdbarang']);
 			$kd_jbrg = $purifier->purify($_POST['kd_jbrg']);
-			$kd_brg = $kd_kbrg+$kd_jbrg;
-			$nm_brg = $purifier->purify($_POST['nm_barang']);
+			$kd_brg = $kdbarang+$kd_jbrg;
+			$nmbarang = $purifier->purify($_POST['nmbarang']);
 			$satuan = $purifier->purify($_POST['satuan']);
 			$kd_perk = $purifier->purify($_POST['kd_perk']);
 			$kd_lokasi = $purifier->purify($_POST['kd_lokasi']);
 
 			$data = array(
-				"kd_kbrg" => $kd_kbrg,
+				"kd_kbrg" => $kdbarang,
 				"kd_jbrg" => $kd_jbrg,
 				"kd_brg" => $kd_brg,
-				"nm_brg" => $nm_brg,
+				"nm_brg" => $nmbarang,
 				"satuan" => $satuan,
 				"kd_perk" => $kd_perk,
 		    	"kd_lokasi" => $kd_lokasi
 		    	 );
-			$Barang->tambahbarang($data);
+			$Barang->tambahbrg($data);
+
+		break;
+
+		case 'readsskel':
+				$kdsskel = $purifier->purify($_POST['kdsskel']);
+				$Barang->$bacasskel($kdsskel);
+			
+			break;
+
+		case 'ubahbarang':
+			$kdbarang = $purifier->purify($_POST['kdbarang']);
+			$kd_jbrg = $purifier->purify($_POST['kd_jbrg']);
+			$kd_brg = $kdbarang+$kd_jbrg;
+			$nmbarang = $purifier->purify($_POST['nmbarang']);
+			$satuan = $purifier->purify($_POST['satuan']);
+			$kd_perk = $purifier->purify($_POST['kd_perk']);
+			$kd_lokasi = $purifier->purify($_POST['kd_lokasi']);
+
+			$data = array(
+				"kd_kbrg" => $kdbarang,
+				"kd_jbrg" => $kd_jbrg,
+				"kd_brg" => $kd_brg,
+				"nm_brg" => $nmbarang,
+				"satuan" => $satuan,
+				"kd_perk" => $kd_perk,
+		    	"kd_lokasi" => $kd_lokasi
+		    	 );
+			$Barang->ubahbarang($data);
+			break;
+
+		case 'hapusbarang':
+			$kdbarang = $purifier->purify($_POST['kdbarang']);
+			$kd_jbrg = $purifier->purify($_POST['kd_jbrg']);
+			$kd_brg = $kdbarang+$kd_jbrg;
+			$nmbarang = $purifier->purify($_POST['nmbarang']);
+			$satuan = $purifier->purify($_POST['satuan']);
+			$kd_perk = $purifier->purify($_POST['kd_perk']);
+			$kd_lokasi = $purifier->purify($_POST['kd_lokasi']);
+
+			$data = array(
+				"kd_kbrg" => $kdbarang,
+				"kd_jbrg" => $kd_jbrg,
+				"kd_brg" => $kd_brg,
+				"nm_brg" => $nmbarang,
+				"satuan" => $satuan,
+				"kd_perk" => $kd_perk,
+		    	"kd_lokasi" => $kd_lokasi
+		    	 );
+			$Barang->hapusbarang($data);
 
 		break;
 		default:
--- core/kanwil/proseskanwil.php
+++ core/kanwil/proseskanwil.php
@@ -10,19 +10,53 @@ else
 	$manage = $_POST['manage'];
 	switch ($manage)
 	{
-			case 'addkanwil':
+		case 'addkanwil':
 			$kodeuapb = $purifier->purify($_POST['kodeuapb']);
-			$kodeuappbe1 = $purifier->purify($_POST['kodeuappbe1']);
+			$kodeuappbe = $purifier->purify($_POST['kodeuappbe']);
 			$kodekanwil = $purifier->purify($_POST['kodekanwil']);
 			$uraiankanwil = $purifier->purify($_POST['uraiankanwil']);
 
 			$data = array(
 				"kd_uapb" => $kodeuapb,
-				"kd_uappbe1" => $kodeuappbe1,
+				"kd_uappbe1" => $kodeuappbe,
 				"kd_kanwil" => $kodekanwil,
 		    	"nm_kanwil" => $uraiankanwil
 		    );
 			$Kanwil->tambahkanwil($data);
+		break;
+
+		case 'readuapb':
+				$kduapb = $purifier->purify($_POST['kduapb']);
+				$Kanwil->$bacauapb($kduapb);
+
+		case 'ubahkanwil':
+			$kodeuapb = $purifier->purify($_POST['kodeuapb']);
+			$kodeuappbe = $purifier->purify($_POST['kodeuappbe']);
+			$kodekanwil = $purifier->purify($_POST['kodekanwil']);
+			$uraiankanwil = $purifier->purify($_POST['uraiankanwil']);
+
+			$data = array(
+				"kd_uapb" => $kodeuapb,
+				"kd_uappbe1" => $kodeuappbe,
+				"kd_kanwil" => $kodekanwil,
+		    	"nm_kanwil" => $uraiankanwil
+		    );
+			$Kanwil->ubahkanwil($data);
+		break;
+
+		case 'hapuskanwil':
+			$kodeuapb = $purifier->purify($_POST['kodeuapb']);
+			$kodeuappbe = $purifier->purify($_POST['kodeuappbe']);
+			$kodekanwil = $purifier->purify($_POST['kodekanwil']);
+			$uraiankanwil = $purifier->purify($_POST['uraiankanwil']);
+
+			$data = array(
+				"kd_uapb" => $kodeuapb,
+				"kd_uappbe1" => $kodeuappbe,
+				"kd_kanwil" => $kodekanwil,
+		    	"nm_kanwil" => $uraiankanwil
+		    );
+			$Kanwil->hapuskanwil($data);
 		break;
 		default:
 			echo "Error Data Tidak Tersedia";
