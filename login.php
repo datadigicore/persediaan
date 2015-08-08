398fc28186c4d1ca0c90a7936128d2435adc43ca
datadigicore <datadigicore@gmail.com>
2015-08-08 09:10:19 +0700

    Fix login validation

--- login.php
+++ login.php
@@ -10,11 +10,6 @@
     <link href="dist/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <link href="dist/css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
-    <link href="dist/css/skins/skin-blue.min.css" rel="stylesheet" type="text/css" />
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]--> 
   </head>
   <header>
   	<div class="headernya">
@@ -27,7 +22,7 @@
     <div class="login-box">
       <div class="login-box-body">
         <p class="login-box-msg">Silahkan isi Username dan Password</p>
-        <form action="config/authenticate" method="post">
+        <form action="config/authenticate" method="post" id="login-form">
           <div class="form-group has-feedback">
             <input type="text" name="username" class="form-control" placeholder="Username" />
             <span class="glyphicon glyphicon-user form-control-feedback"></span>
@@ -39,8 +34,7 @@
           <div class="row">
             <div class="col-xs-8"> 
               <div class="form-group has-feedback">
-                <select id="thn_ang" name="thn_ang" class="form-control">
-                </select>
+                <input id="thn_ang" name="thn_ang" class="form-control"placeholder="Tahun Anggaran"/>
               </div>
             </div>
             <div class="col-xs-4" style="padding-left:0;">
@@ -55,16 +49,39 @@
 	  		<b>Copyright &copy; 2015</b> Team Gunadarma. All Rights Reserved.
 	  	</div>
     </footer>
-    <?php include("include/loadjs.php"); ?>
+    <script type="text/javascript" src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="dist/js/jquery.validate.min.js"></script>
     <script type="text/javascript">
-      var start = new Date().getFullYear()-1;
-      var end = new Date().getFullYear()+1;
-      var options ="<option>-- Tahun Anggaran --</option>";
-      for(var year = start ; year <= end; year++)
-      {
-        options += "<option>"+year+"</option>";
-      }
-      document.getElementById("thn_ang").innerHTML = options;
+      var thisyear = new Date().getFullYear();
+      document.getElementById("thn_ang").value = thisyear;
+      (function($,W,D){
+        var JQUERY4U = {};
+        JQUERY4U.UTIL = {
+          setupFormValidation: function()
+          {
+            $("#login-form").validate({
+                rules: {
+                    username: "required",
+                    password: "required",
+                    thn_ang: "required"
+                },
+                errorPlacement: function (error, element) {
+                    $(element).css({border:"1px solid red"});
+                },
+                success: function (label, element) {
+                    $(element).css({border:"1px solid #ccc"});
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+        }
+    }
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
+    });
+    })(jQuery, window, document);
     </script>
   </body>
 </html>
