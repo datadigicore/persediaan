89b32fdf41f6d86b7e9914b81efeff16952f10b6
datadigicore <datadigicore@gmail.com>
2015-09-30 05:15:08 +0700

    Revisi Alert Trans_Msk

--- user/trans_masuk.php
+++ user/trans_masuk.php
@@ -113,7 +113,13 @@
         $("li#trans_masuk").addClass("active");
         $('#tgl_dok').datepicker({
           format: "dd-mm-yyyy"
-        });         
+        });
+        // $('#tgl_dok').datepicker().change(function() {
+        //   alert("haha");
+        // });
+        $( ".target" ).change(function() {
+          alert( "Handler for .change() called." );
+        });
         $('#tgl_buku').datepicker({
           format: "dd-mm-yyyy"
         });             
@@ -136,122 +142,15 @@
             {"orderable": false,
              "data": null,
              "defaultContent":  '<div class="box-tools">'+
-                                // '<a href="edit_trans_masuk?id=a" class="btn btn-success btn-sm daterange pull-left" role="button"><i class="fa fa-edit"></i></a>'+
-                                  // '<button id="btnedt" class="btn btn-success btn-sm daterange pull-left" data-toggle="tooltip" title="Edit"><i class="fa fa-edit"></i></button>'+
-                                  // '<button id="btntmbh" class="btn btn-success btn-flat btn-xs pull-left"><i class="fa fa-eye"></i> Lihat</button>'+
                                   '<button id="btntmbh" class="btn btn-info btn-flat btn-xs pull-right"><i class="fa fa-plus"></i> Tambah</button>'+
                                 '</div>',
              "targets": [7],"targets": 7 },
-
           ],
         });
         // $('#example1 tbody').on('click', 'tr', function () {
         //     var data = table.row( this ).data();
         //     alert( 'You clicked on '+data[0]+'\'s row' );
         // } );
-        $('#no_dok_item').change(function(){
-        var identtrans = $(this).val();
-        $.ajax({
-          type: "post",
-          url: '../core/transaksi/prosestransaksi',
-          data: {manage:'readidenttrans',idtrans:identtrans},
-          dataType: "json",
-          success: function (output) {
-            // alert(output.jenistrans);
-            // alert(output.tgldok);
-            // alert(output.tglbuku);
-            // alert(output.satker);
-            document.getElementById("jenis_trans").value = "";
-            $('#disnobukti').val(output.nobukti);
-            $('#disjenistrans').val(output.jenistrans);
-            $('#distgldok').val(output.tgldok);
-            $('#distglbuku').val(output.tglbuku);
-            $('#dissatker').val(output.satker);
-            $('#distottrans').val(output.total);
-            $('#jml_msk').prop('required', true);
-            $('#rph_sat').prop('required', true);
-          }
-        });
-      });
-      $('#jenis_trans').change(function(){
-         $("#no_dok_item").select2().select2('val','');
-          $('#disnobukti').val('');
-          $('#disjenistrans').val('');
-           $('#distgldok').val('');
-           $('#distglbuku').val('');
-           $('#dissatker').val('');
-           $('#distottrans').val('');
-            $('#jml_msk').prop('required', false);
-            $('#rph_sat').prop('required', false);
-
-      });  
-      });
-
-      $(document).on('click', '#btnedt', function () {
-        var tr = $(this).closest('tr');
-        var row = table.row( tr );
-        id_row = row.data()[0];
-        nd = row.data()[1];
-        nbukti = row.data()[2];
-        td = row.data()[3];
-        tbuku = row.data()[4];
-        nbrg = row.data()[5];
-        qty = row.data()[6];
-        hrg = row.data()[7];
-        ket = row.data()[9];
-      
-      $.ajax({
-          type: "post",
-          url: '../core/transaksi/prosestransaksi',
-          data: {manage:'cek_brg_masuk',id_row:id_row},
-          dataType: "json",
-          success: function (output) {
-            if(output.st_op==1)
-            {
-              alert("Tidak Dapat Menghapus Barang yang sudah diopname !");
-              return false;
-            }
-            if(output.qty!=null)
-            {
-              alert("Tidak dapat menghapus, barang sudah dikeluarkan pada tanggal "+output.tgl_dok+" sebanyak "+output.qty+" "+output.satuan);
-              return false;
-            }
-            else
-            {
-            job=confirm("Anda yakin ingin menghapus data ini?");
-            if(job!=true)
-            {
-              return false;
-            }
-            else
-            {
-              $('#myModal').modal({
-                backdrop: 'static',
-                keyboard: false
-              });
-              $('#myModal').modal('show');
-              $.ajax({
-                type: "post",
-                url : "../core/transaksi/prosestransaksi",
-                data: {manage:managedata,id:id_row},
-                success: function(data)
-                {
-                  $("#success-alert").alert();
-                  $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-                  $("#success-alert").alert('close');
-                  });
-                  setTimeout("location.href = redirectURL;",redirectTime); 
-                }
-              });
-              return false;
-            }
-            }
-
-        }
-      });
-
-
-      
       });
 
       $(document).on('click', '#btntmbh', function () {
@@ -273,50 +172,14 @@
         $form.submit();
       });
 
-        $.ajax({
-          type: "post",
-          url: '../core/transaksi/prosestransaksi',
-          data: {manage:'readnodok',no_dok:"<?php echo($_SESSION['kd_lok']);?>"},
-          success: function (output) {     
-            $('#no_dok_item').html(output);
-          }
-        });
-        $.ajax({
-          type: "post",
-          url: '../core/transaksi/prosestransaksi',
-          data: {manage:'readsatkerdok',no_dok:"<?php echo($_SESSION['kd_lok']);?>"},
-          success: function (output) {     
-            $('#read_no_dok').html(output);
-          }
-        });
-       $.ajax({
-          type: "post",
-          url: '../core/transaksi/prosestransaksi',
-          data: {manage:'readbrg'},
-          success: function (output) {     
-            $('#kd_brg').html(output);
-          }
-       });
-      $('#kd_brg').change(function(){
-        if ($(this).val()=='') {
-          $('#satuan').val('');
-          
-        }
-        else {
-          var kd_brg = $('#kd_brg').val(); 
-          var no_dok = $('#no_dok').val(); 
-          $.ajax({
-          type: "post",
-          url: '../core/transaksi/prosestransaksi',
-          data: {manage:'baca_detil_trans',kd_brg:kd_brg,no_dok:no_dok},
-          dataType: "json",
-          success: function (output) {     
-            $('#satuan').val(output.satuan);
-          }
-       });
+      $.ajax({
+        type: "post",
+        url: '../core/transaksi/prosestransaksi',
+        data: {manage:'readsatkerdok',no_dok:"<?php echo($_SESSION['kd_lok']);?>"},
+        success: function (output) {     
+          $('#read_no_dok').html(output);
         }
       });
-
 
       $('#addtransmsk').submit(function(e){
         var jns_trans = $("#jenis_trans").val();
@@ -327,18 +190,20 @@
         var tgl_dok = $("#tgl_dok").val();
         var tgl_buku = $("#tgl_buku").val();
         var tgl_terakhir = "";
-        var satkernodok = $("#read_no_dok").val();
-        var disjenistrans = $("#distottrans").val();
-        var distgldok = $("#distgldok").val();
-        var distglbuku = $("#distglbuku").val();
-        var dissatker = $("#dissatker").val();
-        var distottrans = $("#distottrans").val();
-        var no_dok_item = $("#no_dok_item").val();
+        // var satkernodok = $("#read_no_dok").val();
+        // var disjenistrans = $("#distottrans").val();
+        // var distgldok = $("#distgldok").val();
+        // var distglbuku = $("#distglbuku").val();
+        // var dissatker = $("#dissatker").val();
+        // var distottrans = $("#distottrans").val();
+        // var no_dok_item = $("#no_dok_item").val();
         var no_dok = $("#no_dok").val();
 
         if(jns_trans!=""){
-          // alert("Jenis Transaksi Dipilih");
-
+          if(no_dok==""){
+            alert("Silahkan Isi Nomor Dokumen");
+            return false;
+          }
           if(tgl_dok.substring(6,10)!=tahun_ang){
             alert("Tahun Dokumen Tidak Sesuai Dengan Tahun Anggaran");
             return false;
@@ -347,15 +212,7 @@
             alert("Tahun Bukti Tidak Sesuai Dengan Tahun Anggaran");
             return false;
           }
-
-          $('#myModal').modal({
-            backdrop: 'static',
-            keyboard: false
-          });
-          $('#myModal').modal('show');
           e.preventDefault();
-          redirectTime = "1600";
-          redirectURL = "trans_masuk";
           var formURL = $(this).attr("action");
           var addData = new FormData(this);
           $.ajax({
@@ -367,36 +224,6 @@
             processData: false,
             success: function(data)
             {
-              $("#success-alert").alert();
-              $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-              $("#success-alert").alert('close');
-              });
-              setTimeout("$('#myModal').modal('hide');$('#tab_1').removeClass('active');$('#li_tab_1').removeClass('active');$('#tab_2').addClass('active');$('#li_tab_2').addClass('active');",redirectTime);
-              $('#addtransmsk').trigger('reset');
-              var identtran = "<?php echo($_SESSION['kd_lok']).'.'?>"+no_dok;
-              // alert(identtran);
-              $.ajax({
-                type: "post",
-                url: '../core/transaksi/prosestransaksi',
-                data: {manage:'readidenttrans',idtrans:identtran},
-                dataType: "json",
-                success: function (output) {
-                  $('#disnobukti').val(output.nobukti);
-                  $('#disjenistrans').val(output.jenistrans);
-                  $('#distgldok').val(output.tgldok);
-                  $('#distglbuku').val(output.tglbuku);
-                  $('#dissatker').val(output.satker);
-                  $('#distottrans').val(output.total);
-                }
-              });
-              $.ajax({
-                type: "post",
-                url: '../core/transaksi/prosestransaksi',
-                data: {manage:'readnodok',no_dok:"<?php echo($_SESSION['kd_lok']);?>"},
-                success: function (output) {     
-                  $('#no_dok_item').html(output);
-                }
-              });
               $("#example1").DataTable().destroy();
               $("#example1 tbody").empty();
               table = $("#example1").DataTable({
@@ -413,82 +240,16 @@
                    "visible": false },
                   {"targets": 4 },
                   {"targets": 5 },
-                  {"orderable": false,
-                   "data": null,
-                   "defaultContent":  '<div class="box-tools">'+
-                                      // '<a href="edit_trans_masuk?id=a" class="btn btn-success btn-sm daterange pull-left" role="button"><i class="fa fa-edit"></i></a>'+
-                                        // '<button id="btnedt" class="btn btn-success btn-sm daterange pull-left" data-toggle="tooltip" title="Edit"><i class="fa fa-edit"></i></button>'+
-                                        '<button id="btnhps" class="btn btn-danger btn-sm pull-right" data-widget="collapse" data-toggle="tooltip" title="Hapus"><i class="fa fa-remove"></i></button>'+
-                                      '</div>',
-                   "targets": [6],"targets": 6 },
-                ],
-                "dom": '<"row"<"col-sm-6"l><"col-sm-6"f>>t<"row"<"col-sm-6"i><"col-sm-6"p>>',
-              });
-            }
-          });
-          return false;
-        }
-        else if (no_dok_item!="") {
-          
-          if(kd_brg==""){
-            alert("Kode Persediaan Belum Dipilih")
-            return false;
-          }
-
-          $('#myModal').modal({
-            backdrop: 'static',
-            keyboard: false
-          });
-          $('#myModal').modal('show');
-          e.preventDefault();
-          redirectTime = "2600";
-          redirectURL = "trans_masuk";
-          var formURL = $(this).attr("action");
-          var addData = new FormData(this);
-          $.ajax({
-            type: "post",
-            data: addData,
-            url : formURL,
-            contentType: false,
-            cache: false,  
-            processData: false,
-            success: function(data)
-            {
-              $("#success-alert").alert();
-              $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-              $("#success-alert").alert('close');
-              });
-              setTimeout("$('#myModal').modal('hide');$('#tab_1').removeClass('active');$('#li_tab_1').removeClass('active');$('#tab_2').addClass('active');$('#li_tab_2').addClass('active');",redirectTime);
-              $("#example1").DataTable().destroy();
-              $("#example1 tbody").empty();
-              table = $("#example1").DataTable({
-                "processing": false,
-                "serverSide": true,
-                "ajax": "../core/loadtable/loadtransmskitm",
-                "columnDefs":
-                [
-                  {"targets": 0,
-                   "visible": false },
-                  {"targets": 1 },
-                  {"targets": 2 },
-                  {"targets": 3 },
-                  {"targets": 4 },
-                  {"targets": 5 },
                   {"targets": 6 },
-                  {"targets": 7,
-                   "visible": false },
                   {"orderable": false,
                    "data": null,
                    "defaultContent":  '<div class="box-tools">'+
-                                      // '<a href="edit_trans_masuk?id=a" class="btn btn-success btn-sm daterange pull-left" role="button"><i class="fa fa-edit"></i></a>'+
-                                        // '<button id="btnedt" class="btn btn-success btn-sm daterange pull-left" data-toggle="tooltip" title="Edit"><i class="fa fa-edit"></i></button>'+
-                                        '<button id="btnhps" class="btn btn-danger btn-sm pull-right" data-widget="collapse" data-toggle="tooltip" title="Hapus"><i class="fa fa-remove"></i></button>'+
+                                        '<button id="btntmbh" class="btn btn-info btn-flat btn-xs pull-right"><i class="fa fa-plus"></i> Tambah</button>'+
                                       '</div>',
-                   "targets": [8],"targets": 8 },
+                   "targets": [7],"targets": 7 },
                 ],
                 "dom": '<"row"<"col-sm-6"l><"col-sm-6"f>>t<"row"<"col-sm-6"i><"col-sm-6"p>>',
               });
-              // setTimeout("location.href = redirectURL;",redirectTime); 
             }
           });
           return false;
