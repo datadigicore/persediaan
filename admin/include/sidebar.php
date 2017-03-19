739b86e86c9303acb088782d51141d5f1599ccb5
fikrifadlillah <fikri.fadlillah@yahoo.com>
2017-03-19 11:45:11 +0700

    Grouuping menu Admin

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -3,12 +3,17 @@
           <ul class="sidebar-menu">
             <?php foreach ($_SESSION['menu'] as $key => $value) { ?>
             <li class="treeview">
-              <li class="header"><?php echo $value['nama_parent']; ?></li>
-                <!-- <ul class="treeview-menu"> -->
+            <a href="#">
+                <i class="fa fa-list-alt"></i> <span><?php echo $value['nama_parent']; ?></span>
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
                 <?php foreach ($value['list_menu'] as $key => $list) { ?>
                   <li id="<?php echo $list['nama_file']; ?>"><a href="<?php echo $list['nama_file']; ?>"><span><?php echo $list['nama_menu']; ?></span></a></li>
                 <?php } ?>
-                <!-- </ul> -->
+                </ul>
               </li>
             <?php } ?>
           </ul>
